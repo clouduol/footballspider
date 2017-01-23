74116846e4bb3d7bdbf6f7ebd5e1006100f87793
duye <guoyl1814@gmail.com>
2017-01-23 11:59:34 +0800

    NavigableString

--- syntaxTest.py
+++ syntaxTest.py
@@ -1,7 +1,20 @@
-def error():
-	try:
-		print(10/0)
-	except ZeroDivisionError as e:
-		print(e)
-	print("hello")
-error()
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from bs4 import NavigableString
+import re
+html = urlopen("file:///home/guoyunlong/workspace/footballspider/index.html")
+bsObj = BeautifulSoup(html, "html.parser")
+
+count = 0
+for child in bsObj.html.body.table.findAll("",limit=6):
+    count=count+1
+    print(count)
+    print(type(child))
+    print(child)
+print(count)
+
+text_list = bsObj.findAll(text=re.compile("team"),limit=1)
+for text in text_list:
+    print(text)
+print(len(text_list))
+
